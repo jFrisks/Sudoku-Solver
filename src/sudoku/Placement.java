ef5d05cfd319c4d8dd6225bee5c34b58a6cfc325
Jonathan Frisk <jontesport@gmail.com>
2018-03-01 11:12:50 +0100

    Sudoku struktur

---
+++ src/sudoku/Placement.java
@@ -0,0 +1,20 @@
+package sudoku;
+
+public class Placement {
+
+    /**
+     * isLegal
+     * @returns true if placement is legal, otherwhise false.
+     * */
+    public static boolean isLegal() {
+        return false;
+    }
+
+    private static boolean boxIsLegal() {
+
+    }
+
+    private static boolean lineIsLegal() {
+
+    }
+}
---
+++ src/sudoku/SudokuSolver.java
@@ -0,0 +1,30 @@
+package sudoku;
+
+public class SudokuSolver {
+
+    /**---- CONSTRUCTOR ----*/
+    public SudokuSolver() {
+
+    }
+
+    /**------- MAIN --------*/
+    public static void main(String[] args) {
+        // write your code here
+    }
+
+
+    /**----- METHODS ------*/
+
+    /**
+     * SOLVE
+     * Tries a placement and see if its legal
+     * @returns true or false depending if successful
+     * Goal is to make whole board successfull and filled up
+     * i -> x-axis
+     * j -> y-axis
+     * */
+    private boolean solve(int i, int j) {
+        return false;
+    }
+
+}
