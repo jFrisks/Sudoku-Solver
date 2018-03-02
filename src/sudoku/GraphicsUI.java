00e3351632940a1d7925bd3274f059b162a1d84c
Jonathan Frisk <jontesport@gmail.com>
2018-03-02 11:48:29 +0100

    Added GraphicsUI File

---
+++ src/sudoku/GraphicsUI.java
@@ -0,0 +1,16 @@
+package sudoku;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class GraphicsUI extends Application {
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) {
+
+    }
+}
