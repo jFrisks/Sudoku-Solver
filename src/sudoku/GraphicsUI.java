e2bf211bf72878eb4339cbf8053191d6634ceac5
Fredrik Nilsson <33512977+Fredddaan@users.noreply.github.com>
2018-03-05 13:22:44 +0100

    Update GraphicsUI.java

--- src/sudoku/GraphicsUI.java
+++ src/sudoku/GraphicsUI.java
@@ -12,11 +12,12 @@ import javafx.scene.layout.TilePane;
 import javafx.stage.Stage;
 
 public class GraphicsUI extends Application {
-		Button B = new Button("Solve");
-		Button C = new Button("Clear");
-		TilePane topbox = new TilePane();
-		BorderPane root = new BorderPane();
-		SudokuSolver solver = new SudokuSolver();
+	Button B = new Button("Solve");
+	Button C = new Button("Clear");
+	TilePane topbox = new TilePane();
+	BorderPane root = new BorderPane();
+	SudokuSolver solver = new SudokuSolver();
+
 	public static void main(String[] args) {
 		launch(args);
 	}
@@ -40,77 +41,96 @@ public class GraphicsUI extends Application {
 		int counter = 0;
 		for (int i = 0; i < 9; i++) {
 			for (int k = 0; k < 9; k++) {
-				counter ++;
-				
+				counter++;
+
 				TextField label = new TextField();
 				label.setAlignment(Pos.CENTER);
 				label.setPrefSize(SIZE, SIZE);
-				
+
 				if (((k < 3 || k > 5) && (i < 3 || i > 5)) || ((i > 2 && i < 6) && (k > 2 && k < 6))) {
 					label.setStyle("-fx-border-color : black;" + "-fx-font-size: 16;");
 
-				}
-				else{
-					label.setStyle("-fx-background-color : pink;" + "-fx-border-color : black;"+ "-fx-font-size: 16;");
+				} else {
+					label.setStyle("-fx-background-color : pink;" + "-fx-border-color : black;" + "-fx-font-size: 16;");
 				}
 				topbox.getChildren().add(label);
-				
-				
+
 			}
 		}
-		
+
 		primaryStage.setScene(scene);
 		primaryStage.show();
 		primaryStage.setResizable(false);
-		
-		
-		B.setOnAction(event -> {
-		//läsa in alla inputs i en array
-			//sedan skall denna översättas till barnen i topboxen 
+		C.setOnAction(event ->{	
+			writeToWindow(clearWindow());
 			
-		
 		});
+		B.setOnAction(event -> {
+			Board board = getFromWindow();
+			solver.solve(board);
+			 writeToWindow(board);
+			System.out.println(getFromWindow().get(0, 0));
+			//TextField label = (TextField) topbox.getChildren().get(0);
+			// System.out.println(label.getText().toCharArray());
+
+			// läsa in alla inputs i en array
+			// sedan skall denna översättas till barnen i topboxen
+
+		});
+
 	}
-	
-	public Board getFromWindow(){
-<<<<<<< HEAD
-		return null;
-=======
+
+	public Board getFromWindow() {
 		int value;
 		int[] array = new int[81];
 		String text;
-		for(int i = 0; i <=81; i++) {
-            
-            	TextField label = (TextField) topbox.getChildren().get(i);
-        		text = label.getText();
-        		if(text == ""){
-        			text = "0";
-        		}
-        		value = Integer.parseInt(text);
-        		
-        		if(value>=0){
-        			array[i] = value;	
-        		}
-        		
-            	
-                
-            }
-        Board board = new Board();
-        int index = 0;
-        for (int i = 0; i < 9; i++) {
+		for (int i = 0; i < 81; i++) {
+			TextField label = (TextField) topbox.getChildren().get(i);
+			//TODO: felinmatning
+			//när ruta ej inmatad
+			text = label.getText();
+			if (text.equals("")) {
+				value = 0;
+				
+			}
+			else{
+				value = Integer.parseInt(label.getText());
+			}
+
+			array[i] = value;
+
+		}
+		Board board = new Board();
+		int index = 0;
+		for (int i = 0; i < 9; i++) {
 			for (int k = 0; k < 9; k++) {
+				// TODO; hantera felinmatade
 				board.set(k, i, array[index]);
 				index++;
 			}
-        }	
+		}
 		return board;
->>>>>>> origin/master
+	}
+
+	public boolean writeToWindow(Board board) {
+	
+
+		int index = 0;
+		for (int y = 0; y < 9; y++) {
+			for (int x = 0; x < 9; x++) {
+				TextField label = (TextField) topbox.getChildren().get(index);
+
+				label.setText(Integer.toString(board.get(x, y)));
+				index++;
+			}
+		}
+
+		return false;
 	}
 	
-	public boolean writeToWindow(Board board){
-		Board board1 = getFromWindow();
+	public Board clearWindow(){
+		Board clearBoard = new Board();
 		
-		return solver.solve(board1);
-		
+		return clearBoard;
 	}
 }
